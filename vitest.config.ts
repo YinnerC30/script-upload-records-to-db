62e60c0387df1cf5a6b58f0bace40aa31a458756
Yinner Chilito <yinnermedina30@gmail.com>
2025-08-21 18:45:24 -0500

    chore(deps): eliminar dependencias innecesarias y ajustar configuración de pruebas
    - Se eliminan dependencias de `@testing-library/react` y `jsdom` del archivo `package.json` y `package-lock.json`.
    - Se actualiza la configuración de Vitest para utilizar el entorno `node` en lugar de `jsdom`.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'node',
     setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
