ce60584f3b84998e2c5c174b58ad4f5afcc16486
Yinner Chilito <yinnermedina30@gmail.com>
2025-10-16 15:38:06 -0500

    fix: agregar cuerpo de respuesta en el manejo de errores en ApiService
    - Se ha añadido el cuerpo de la respuesta (`body`) en el objeto de error dentro de `ApiService`, mejorando la información disponible para el manejo de errores y facilitando la depuración.

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -245,6 +245,7 @@ export class ApiService {
           licitacion_id: licitacion.licitacion_id,
           error: error.message,
           statusCode: error.response?.status,
+          body: JSON.stringify(licitacion),
         },
         error.response?.data ? { ...error.response.data } : {}
       );
